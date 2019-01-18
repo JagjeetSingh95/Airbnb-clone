5888ae539b5cdc0998ffeaa132de3fe7e46e75b0
Jagjeet Singh <jagsingh.96@gmail.com>
2019-01-19 01:01:06 +0530

    Home design with reusble colors and components

--- src/components/buttons/RoundedButton.js
+++ src/components/buttons/RoundedButton.js
@@ -7,25 +7,48 @@ import {
   StyleSheet
 } from 'react-native';
 
+import colors from '../../styles/colors';
+
 export default class RoundedButton extends Component {
   render() {
-    const { text } = this.props;
+    const { text, textColor, background, handleOnPress, icon } = this.props;
+    const backgroundColor = background || 'transparent';
+    const color = textColor || colors.green01;
     return (
-      <TouchableHighlight>
-        <Text style={styles.buttonText}>{text}</Text> 
+      <TouchableHighlight
+        onPress={handleOnPress} 
+        style={[{backgroundColor}, styles.wrapper]}
+        >
+        <View style={styles.buttonTextWrapper}>
+          {icon}
+          <Text style={[{color}, styles.buttonText]}>{text}</Text> 
+        </View>
       </TouchableHighlight>
     );
   }
 }
 
 RoundedButton.PropTypes =  {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  icon: PropTypes.object,
+  handleOnPress: PropTypes.func.isRequired
 }
 
 const styles = StyleSheet.create({
   wrapper: {
     display: 'flex',
-    padding: 15
+    padding: 15,
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: colors.white,
+    marginBottom: 15,
+    alignItems: 'center'
+  },
+  buttonTextWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
   },
   buttonText: {
     fontSize: 17,
--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -3,24 +3,78 @@ import {
   StyleSheet, 
   Text, 
   View,
-  Image
-
+  Image,
+  TouchableHighlight
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import colors from '../styles/colors';
 import RoundedButton from '../components/buttons/RoundedButton';
 
 export default class LoggedOut  extends Component {
+  onFacebookPress() {
+    alert('Facebook Button Pressed!')
+  }
+  onCreateAccoundPress() {
+    alert('Create Account Button Pressed!')
+  }
+  onMoreOptionsPress() {
+    alert('More Options Button Pressed!')
+  }
+
   render() {
     return (
       <View style={styles.wrapper}>
         <View style={styles.welcomeWrapper}>
+        
           <Image 
             source={require('../img/airbnd-logo.png')}
             style={styles.logo} 
             />  
+
           <Text style={styles.welcomeText}>Welcome to Airbnb.</Text>
-          <RoundedButton text="Countinue with Facebook" />
+
+          <RoundedButton 
+            text="Continue with Facebook" 
+            textColor={colors.green01}
+            background={colors.white}
+            icon={<Icon name="facebook" size={20} style={styles.facebookIcon} />}
+            handleOnPress={this.onFacebookPress}
+            />
+
+          <RoundedButton 
+            text="Create Account" 
+            textColor={colors.white}
+            background={colors.green01}
+            handleOnPress={this.onCreateAccoundPress}
+            />
+
+          <TouchableHighlight 
+            style={styles.moreOptionsButton}
+            onPress={this.onMoreOptionsPress}
+            >
+            <Text style={styles.moreOptionsButtonText}>More options </Text>
+          </TouchableHighlight>
+
+          <View style={styles.termsAndConditions}>
+            <Text style={styles.termsText}>By tapping Continue, Create Account or More</Text>
+            <Text style={styles.termsText}>options, </Text>
+            <Text style={styles.termsText}>I am agree to Airbnb </Text>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>Term of Service, </Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>Payment Terms of Service, </Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>Privacy Policy</Text>
+            </TouchableHighlight>
+            <Text style={styles.termsText}>, and </Text>
+            <TouchableHighlight style={styles.linkButton}>
+              <Text style={styles.termsText}>Nondiscrimination Policy.</Text>
+            </TouchableHighlight>
+          </View>
+
         </View>
       </View>
     );
@@ -50,5 +104,33 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontWeight: '300',
     marginBottom: 30
+  },
+  facebookIcon: {
+    color: colors.green01,
+    position: 'relative',
+    left: 20,
+    zIndex: 8
+  },
+  moreOptionsButton: {
+    marginTop: 15 
+  },
+  moreOptionsButtonText: {
+    color: colors.white,
+    fontSize: 16
+  },
+  termsAndConditions: {
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    marginTop: 30
+  },
+  termsText: {
+    color: colors.white,
+    fontSize: 13,
+    fontWeight: '600'
+  },
+  linkButton: {
+    borderBottomWidth: 1,
+    borderBottomColor: colors.white 
   }
 });
